14bf2ad66f1b3705f86fca6b206632a74eaf8e1b
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-06-21 21:38:51 +0600

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -6,5 +6,5 @@ def register(app):
     def start_command(client, message):
         client.send_message(
             chat_id=message.chat.id,
-            text='Hello! - Iam a Powerfull Checker'
+            text='Iam Alive'
         )
