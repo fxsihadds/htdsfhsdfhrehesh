22f47c3bb7b04ec68125c0ef93da131c5ce23d24
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-09-07 13:03:51 +0600

    Add files via upload

---
+++ handlers/rnd.py
@@ -0,0 +1,38 @@
+import requests
+from pyrogram import Client, filters
+
+
+def register(app):
+    @app.on_message(filters.command("rand"))
+    async def rand_command(_, message):
+        await rand_helper_command(_, message)
+
+
+async def rand_helper_command(_, message):
+    api = requests.get("https://randomuser.me/api/?nat=us&inc=name,location").json()
+
+    mr = api["results"][0]["name"]["title"]
+    nombre = api["results"][0]["name"]["first"]
+    last = api["results"][0]["name"]["last"]
+    loca = api["results"][0]["location"]["street"]["name"]
+    nm = api["results"][0]["location"]["street"]["number"]
+    city = api["results"][0]["location"]["city"]
+    state = api["results"][0]["location"]["state"]
+    country = api["results"][0]["location"]["country"]
+    postcode = api["results"][0]["location"]["postcode"]
+    latitude = api["results"][0]["location"]["coordinates"]["latitude"]
+    longitude = api["results"][0]["location"]["coordinates"]["longitude"]
+
+    await message.reply(f"""
+<b> 
+⎚ 𝐅𝐚𝐤𝐞 𝐀𝐝𝐝𝐫𝐞𝐬𝐬
+⎚ 𝐍𝐚𝐦𝐞: <code>{mr} {nombre} {last}</code>
+⎚ 𝐒𝐭𝐫𝐞𝐞𝐭:  <code>{state}</code>
+⎚ 𝐂𝐢𝐭𝐲: <code>{city}</code>
+⎚ 𝐒𝐭𝐚𝐭𝐞:<code> {loca} {nm}</code>
+⎚ 𝐏𝐨𝐬𝐭𝐜𝐨𝐝𝐞: <code> {postcode}</code>
+⎚ 𝐂𝐨𝐮𝐧𝐭𝐫𝐲: <code>{country}</code>
+⎚ 𝐂𝐡𝐞𝐜𝐤𝐞𝐝 𝐁𝐲 <code> @{message.from_user.username}</code>
+━━━━━━━━━━━━━━
+⎚ Create <b><a href="tg://resolve?domain=RexAwait">𝗿𝗲𝘅 hᴀᴡᴀɪɪ |「💻」</a></b>
+ </b>""")
