616acb621feb994c8c97d8aedff51d0b8224974d
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-09-25 14:57:32 +0600

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -6,16 +6,16 @@ def register(app):
     def help_command(client, message):
         client.send_message(
             chat_id=message.chat.id,
-            text = 'Available commands:\n' \
-                   '<code>/start</code> - <b>Start the bot</b>\n' \
-                   '<code>/bin</code> - <b>Check Your Bin</b>\n' \
-                   '<code>/Gscraper</code> - <b>Combo Scrape</b>\n' \
-                   '<code>/paste</code> - <b>Paste Any text</b>\n' \
-                   '<code>/unzip</code> - <b>Unzip Your File</b>\n' \
-                   '<code>/hoichoi</code> - <b>Check Your Valid Hoichoi Combo</b>\n' \
-                   '<code>/crunchyroll</code> - <b>Check Your Valid Crunchyroll Combo</b>\n' \
-                   '<code>/chaupal</code> - <b>Check Your Valid chaupal Combo</b>\n' \
-                   '<code>/ip</code> - <b>Check Your IP address</b>\n' \
-                   '<code>/rand</code> - <b>Fake Random Details</b>\n' \
+            text = '<b>Available commands:</b>\n'
+                   '<code>/start</code> - <b>Start the bot</b>\n'
+                   '<code>/bin</code> - <b>Check Your Bin</b>\n'
+                   '<code>/Gscraper</code> - <b>Combo Scrape</b>\n'
+                   '<code>/paste</code> - <b>Paste Any text</b>\n'
+                   '<code>/unzip</code> - <b>Unzip Your File</b>\n'
+                   '<code>/hoichoi</code> - <b>Check Your Valid Hoichoi Combo</b>\n'
+                   '<code>/crunchyroll</code> - <b>Check Your Valid Crunchyroll Combo</b>\n'
+                   '<code>/chaupal</code> - <b>Check Your Valid chaupal Combo</b>\n'
+                   '<code>/ip</code> - <b>Check Your IP address</b>\n'
+                   '<code>/rand</code> - <b>Fake Random Details</b>\n'
                    '<code>/ocr</code> - <b>Images OCR</b>\n'
         )
