083f5da821057a7415719062cc355020ef790e80
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-10-01 20:44:38 +0600

    Update userinfo.py

--- handlers/userinfo.py
+++ handlers/userinfo.py
@@ -1,22 +1,30 @@
-from pyrogram import Client, filters
-
-
+from pyrogram import Client,filters
 def register(app):
-    @app.on_message(filters.command("userinfo"))
-    async def userinfo_command(client: Client, message):
-        if len(message.command) > 1:
-            username = message.command[1]
-            try:
-                user = await app.get_users(username)
-                await message.reply_text(
-                    f"User ID: {user.id}\nUsername: {user.username}\nFirst Name: {user.first_name}")
-            except Exception as e:
-                await message.reply_text(f"An error occurred while fetching user info: {e}")
-        elif message.reply_to_message:
-            user_id = message.reply_to_message.from_user.id
-            username = message.reply_to_message.from_user.username
-            first_name = message.reply_to_message.from_user.first_name
-
-            await message.reply_text(f"User ID: {user_id}\nUsername: {username}\nFirst Name: {first_name}")
-        else:
-            await message.reply_text("Please reply to a user's message or provide a username to get user information.")
+	@app.on_message(filters.command('id',['/','.']))
+	async def A(client,message):
+		A=message
+		if len(A.command)>1:
+			C=A.command[1]
+			try:B=await app.get_users(C);D=f"""<strong>User Information: </strong>
+━━━━━━━━━━━
+<b>User ID:</b> <code>{B.id}</code>
+<b>Username:</b> <code>{B.username}</code>
+<b>First Name:</b> <code>{B.first_name}</code>
+<b>Last Name:</b> <code>{B.last_name}</code>
+<b>DC ID:</b> <code>{B.dc_id}</code>
+<b>Is Bot:</b> <code>{B.is_bot}</code>
+<b>Language Code:</b> <code>{B.language_code}</code>
+<b>Last Online Date:</b> <code>{B.last_online_date}</code>
+""";await A.reply_text(D)
+			except Exception as E:await A.reply_text(f"An error occurred while fetching user info: {E}")
+		else:F=A.from_user.id;C=A.from_user.username;G=A.from_user.first_name;H=A.from_user.last_name;I=A.from_user.dc_id;J=A.from_user.is_bot;K=A.from_user.language_code;L=A.from_user.last_online_date;D=f"""<strong>User Information: </strong>
+━━━━━━━━━━━
+<b>User ID:</b> <code>{F}</code>
+<b>Username:</b> <code>{C}</code>
+<b>First Name:</b> <code>{G}</code>
+<b>Last Name:</b> <code>{H}</code>
+<b>DC ID:</b> <code>{I}</code>
+<b>Is Bot:</b> <code>{J}</code>
+<b>Language Code:</b> <code>{K}</code>
+<b>Last Online Date:</b> <code>{L}</code>
+""";await A.reply_text(D)
