c52eb0963e4ae3aa0a502d52d58eac9770c58798
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-10-01 09:18:18 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,14 @@
-import configparser
-import os
+_A='pyrogram'
+import configparser,os
 from pyrogram import Client
-
-# Load configuration
-config = configparser.ConfigParser()
+config=configparser.ConfigParser()
 config.read('config.ini')
-
-# Create a Pyrogram client instance
-app = Client(
-    'my_bot',
-    api_id=config['pyrogram']['api_id'],
-    api_hash=config['pyrogram']['api_hash'],
-    bot_token=config['pyrogram']['bot_token']
-)
-
-# Load command handlers from the 'handlers' folder
-handlers_dir = 'handlers'
-users_dir = 'users'
-
-# Iterate through the 'handlers' folder
+app=Client('my_bot',api_id=config[_A]['api_id'],api_hash=config[_A]['api_hash'],bot_token=config[_A]['bot_token'])
+handlers_dir='handlers'
+users_dir='users'
 for filename in os.listdir(handlers_dir):
-    if filename.endswith('.py'):
-        module_name = filename[:-3]
-        module = __import__(f'{handlers_dir}.{module_name}', fromlist=[module_name])
-        module.register(app)
-
-# Iterate through the 'users' folder
+	if filename.endswith('.py'):module_name=filename[:-3];module=__import__(f"{handlers_dir}.{module_name}",fromlist=[module_name]);module.register(app)
 for filename in os.listdir(users_dir):
-    if filename.endswith('.py'):
-        module_name = filename[:-3]
-        module = __import__(f'{users_dir}.{module_name}', fromlist=[module_name])
-        module.register(app)
-
-# Run the bot
-print("BOT Alive")
+	if filename.endswith('.py'):module_name=filename[:-3];module=__import__(f"{users_dir}.{module_name}",fromlist=[module_name]);module.register(app)
+print('BOT Alive')
 app.run()
