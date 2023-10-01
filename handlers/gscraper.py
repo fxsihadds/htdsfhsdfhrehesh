d490e14cbc7bfaee622a9d1ed881c0e5a8341bcf
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-10-01 14:56:18 +0600

    Update gscraper.py

--- handlers/gscraper.py
+++ handlers/gscraper.py
@@ -1,62 +1,28 @@
-import re
-import os
+import re,os
 from pyrogram import filters
-
-
-def extract_emails(text):
-    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}:[^\s]+\b'
-    emails = re.findall(pattern, text)
-    return emails
-
-
+EMAIL_PASSWORD_REGEX='\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}:[A-Za-z0-9]+\\b'
+def extract_email_passwords(text):A=re.findall(EMAIL_PASSWORD_REGEX,text);return A
 def register(app):
-    @app.on_message(filters.command('gscraper'))
-    async def scraper_command(client, message):
-        status = await message.reply("<b>⎚ `Extracting...`</b>")
-        if message.reply_to_message is not None and message.reply_to_message.document:
-            # Scraping from a replied document
-            document = message.reply_to_message.document
-
-            if document.file_name.endswith('.txt'):
-                # Check if the file is a text file
-                file_path = await client.download_media(document)
-
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    text_to_scrape = file.read()
-
-                # Remove the temporary file
-                os.remove(file_path)
-            else:
-                await status.edit_text(text='<b>Please provide a text file (.txt) to scrape Gmail:Password😞.</b>')
-                return
-        else:
-            # Scraping from the command message
-            command_text = message.text.split(' ', 1)
-            if len(command_text) > 1:
-                text_to_scrape = command_text[1]
-            else:
-                await status.edit_text(text='<b>Please provide a text file (.txt) to scrape Gmail:Password😞.</b>')
-                return
-
-        emails = extract_emails(text_to_scrape)
-
-        if len(emails) > 0:
-            # Create a file with the extracted emails
-            filename = 'Gmail:Password.txt'
-            with open(filename, 'w') as file:
-                for email in emails:
-                    file.write(email + '\n')
-
-            # Reply with the file
-            await client.send_document(
-                chat_id=message.chat.id,
-                document=filename,
-                caption=f'<b>Extracted {len(emails)}</b>'
-            )
-
-            # Remove the file after sending
-            os.remove(filename)
-            await status.delete()
-
-        else:
-            await status.edit_text(text='<b>No email addresses found in the provided text ❌.</b>')
+	@app.on_message(filters.command('gscraper'))
+	async def A(client,message):
+		H=client;G=None;A=message;C=await A.reply('<b>⎚ `Extracting...`</b>');B=G
+		if A.reply_to_message:
+			if A.reply_to_message.document:
+				I=A.reply_to_message.document
+				if I.file_name.endswith('.txt'):
+					J=await H.download_media(I)
+					with open(J,'r',encoding='utf-8')as D:B=D.read()
+					os.remove(J)
+				else:await C.edit_text(text='<b>Please provide a text file (.txt) to scrape email:password combinations😞.</b>');return
+			elif A.reply_to_message.text:B=A.reply_to_message.text
+		if B is G:
+			K=A.text.split(' ',1)
+			if len(K)>1:B=K[1]
+		if B is G:await C.edit_text(text='<b>No text found to scrape email:password combinations😞.</b>');return
+		E=extract_email_passwords(B)
+		if len(E)>0:
+			F='Email_Password.txt'
+			with open(F,'w')as D:
+				for L in E:D.write(L+'\n')
+			await H.send_document(chat_id=A.chat.id,document=F,caption=f"<b>Extracted {len(E)} email:password</b>");os.remove(F);await C.delete()
+		else:await C.edit_text(text='<b>No email:password combinations found in the provided text ❌.</b>')
