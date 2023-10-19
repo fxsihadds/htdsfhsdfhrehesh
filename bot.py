bd2c1ed4f8b7ac0ec50cd349c7f3dac4a3a70158
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-10-19 13:17:32 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,61 @@
-_A='pyrogram'
-import configparser,os
+import configparser
+import os
+import logging
 from pyrogram import Client
-config=configparser.ConfigParser()
+from pyrogram.types import Message
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
+logging.getLogger(__name__)
+
+
+config = configparser.ConfigParser()
 config.read('config.ini')
-app=Client('my_bot',api_id=config[_A]['api_id'],api_hash=config[_A]['api_hash'],bot_token=config[_A]['bot_token'], workers=20)
-handlers_dir='handlers'
-users_dir='users'
+
+plugins = dict(
+    root="plugins",
+)
+
+
+handlers_dir = 'handlers'
+checkers_dir = 'checkers'
+users_dir = 'users'
+
+
+app = Client(
+    'my_bot',
+    api_id=config['pyrogram']['api_id'],
+    api_hash=config['pyrogram']['api_hash'],
+    bot_token=config['pyrogram']['bot_token'],
+    plugins=plugins,
+    workers=10
+)
+
+
+for filename in os.listdir(checkers_dir):
+    if filename.endswith('.py'):
+        module_name = filename[:-3]
+        module = __import__(f'{checkers_dir}.{module_name}',
+                            fromlist=[module_name])
+        module.register(app)
+
+
 for filename in os.listdir(handlers_dir):
-	if filename.endswith('.py'):module_name=filename[:-3];module=__import__(f"{handlers_dir}.{module_name}",fromlist=[module_name]);module.register(app)
+    if filename.endswith('.py'):
+        module_name = filename[:-3]
+        module = __import__(
+            f'{handlers_dir}.{module_name}', fromlist=[module_name])
+        module.register(app)
+
 for filename in os.listdir(users_dir):
-	if filename.endswith('.py'):module_name=filename[:-3];module=__import__(f"{users_dir}.{module_name}",fromlist=[module_name]);module.register(app)
-print('BOT Alive')
+    if filename.endswith('.py'):
+        module_name = filename[:-3]
+        module = __import__(f'{users_dir}.{module_name}',
+                            fromlist=[module_name])
+        module.register(app)
+
+
 app.run()
