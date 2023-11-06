bef0480a5ed607d50084a70ca0cbff00a11811c8
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2023-11-06 21:11:53 +0600

    Update hachoir.py

--- plugins/hachoir.py
+++ plugins/hachoir.py
@@ -1,6 +1,8 @@
 from PIL import Image
 from random import randint
 from moviepy.editor import VideoFileClip
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import os
 
 
 def get_video_duration(video_file_path):
@@ -26,6 +28,40 @@ def thumbnail_video(video_file_path):
         print(f"Error: {e}")
 
     return
+
+
+def split_scene(video_file_path) -> bool:
+    if os.path.isfile(video_file_path):
+        get_size = os.path.getsize(video_file_path)
+        file_size_megabytes = round(get_size / (1024 * 1024))
+        print(f"File size: {file_size_megabytes} MB")
+        if file_size_megabytes > 1950:
+            durations = get_video_duration(video_file_path)
+            num_segments = 2
+            segment_duration = durations / num_segments
+            output_directory = "your_download"
+            os.makedirs(output_directory, exist_ok=True)
+
+            for i in range(num_segments):
+                start_time = i * segment_duration
+                end_time = (i + 1) * segment_duration
+                output_file = os.path.join(
+                    output_directory, f"{os.path.basename(video_file_path)}_{i + 1}.mp4")
+
+                with open(video_file_path, 'rb') as video_file:
+                    ffmpeg_extract_subclip(
+                        video_file_path, start_time, end_time, targetname=output_file)
+
+            print("Video split into segments.")
+            os.remove(video_file_path)
+            return True
+        else:
+            return False
+            print("File is small enough to upload to Telegram.")
+    else:
+        return False
+        print("File not found.")
+
 
 def get_file_size(video_file_path) -> bool:
     get_size = os.path.getsize(video_file_path)
